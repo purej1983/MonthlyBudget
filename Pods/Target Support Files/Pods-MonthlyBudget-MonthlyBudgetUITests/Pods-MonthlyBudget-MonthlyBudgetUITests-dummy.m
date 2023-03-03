4015dd5103ec63cc8e883ad5d6a0d58084c9cae7
Thomas Lam <purej1983@gmail.com>
2023-03-03 10:08:45 +0000

    build: cocoapod init

---
+++ Pods/Target Support Files/Pods-MonthlyBudget-MonthlyBudgetUITests/Pods-MonthlyBudget-MonthlyBudgetUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MonthlyBudget_MonthlyBudgetUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MonthlyBudget_MonthlyBudgetUITests
+@end
